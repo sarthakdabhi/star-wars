b70d6e9a91d85c757b06b3a598e304c73d5d5f7a
Sarthak Dabhi <dabhi.sarthak@gmail.com>
2018-01-02 01:54:26 -0800

    The Force

--- src/main.ts
+++ src/main.ts
@@ -117,19 +117,19 @@ class App {
                             'bottom': '600px'
                         }, 5000, function() {
                             that.quesCount = 0;
-                            // $('.screens').hide();
-                            // const screen2El = $('#screen-3');
-                            // screen2El.show();
-                            // $('#points').html(String(that.points) + "/" + String(that.queans.length));
-                            // $('#answer-a').off('click');
-                            // $('#answer-a').on('click', function() {
-                            //     that.checkAns('a');
-                            // });
-                            // $('#answer-b').off('click');
-                            // $('#answer-b').on('click', function() {
-                            //     that.checkAns('b');
-                            // });
-                            // that.newQuestion();
+                            $('.screens').hide();
+                            const screen2El = $('#screen-3');
+                            screen2El.show();
+                            $('#points').html(String(that.points) + "/" + String(that.queans.length));
+                            $('#answer-a').off('click');
+                            $('#answer-a').on('click', function() {
+                                that.checkAns('a');
+                            });
+                            $('#answer-b').off('click');
+                            $('#answer-b').on('click', function() {
+                                that.checkAns('b');
+                            });
+                            that.newQuestion();
                         });
                     });
                 }, 5000);
